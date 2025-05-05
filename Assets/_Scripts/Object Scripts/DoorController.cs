e1ec819fe5f20108ca6125ca9db93412a80bce8b
bwbelcher12 <bwbelcher12@gmail.com>
2025-05-05 16:13:13 -0400

    Fixed door network behavior

--- Assets/_Scripts/Object Scripts/DoorController.cs
+++ Assets/_Scripts/Object Scripts/DoorController.cs
@@ -3,9 +3,46 @@ using Mirror;
 
 public class DoorController : InteractableObject
 {
+    [SerializeField] NetworkAnimator n_animator;
+    [SerializeField] PlayOneshot doorSound;
+
     [Command]
     public new void Interact()
     {
         Debug.Log("Hello connection " + connectionToClient);
+    }
+
+
+    private bool doorState;
+
+    private void Start()
+    {
+        doorState = n_animator.animator.GetBool("Open");
+    }
+
+    public void InteractCalled()
+    {
+        Debug.Log("Interaction called from " + connectionToClient);
+    }
+
+    public void ToggleDoor()
+    {
+        n_animator.animator.SetBool("Open", !doorState);
+        doorState = !doorState;
+    }
+
+    [Command]
+    public void CmdPlaySound()
+    {
+        if (isServer)
+            return;
+
+        RpcPlaySound();
+    }
+    
+    [ClientRpc]
+    public void RpcPlaySound()
+    {
+        doorSound.PlayClip();
     }
 }
--- Assets/_Scripts/Object Scripts/InteractableButton.cs
+++ Assets/_Scripts/Object Scripts/InteractableButton.cs
@@ -3,10 +3,5 @@ using Mirror;
 
 public class InteractableButton : InteractableObject
 {
-    [SerializeField] MeshCollider buttonCollider;
 
-    public void InteractCalled()
-    {
-        Debug.Log("Interaction called from " + connectionToClient);
-    }
 }
